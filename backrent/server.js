3f13c5fc28b92ee2924c7da72d83a04ec2904ad8
devyank <devyanknagpal2002@gmail.com>
2022-12-30 21:43:14 +0530

    get req

--- backrent/server.js
+++ backrent/server.js
@@ -1,8 +1,12 @@
 const express =require("express")
 const app=express();
+const cors=require("cors");
 const dotenv=require("dotenv").config()
 const mongoose=require("mongoose")
 const errorHandler=require("./middleware/errorHandler")
+// const bodyparser=require("body-parser");
+const bodyParser = require("body-parser");
+
 const port=process.env.PORT||3000
 
 
@@ -10,6 +14,9 @@ mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://localhost/products")
 
 app.use(express.json())
+app.use(cors())
+// app.use(bodyParser.json({limit:"30mb",extended:true}))
+// app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 app.use("/api/rent",require("./router/rentRouter"))
 app.use("/api/user",require("./router/userRouter"))
 app.use(errorHandler)
--- frontrent/src/components/Register.jsx
+++ frontrent/src/components/Register.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+import axios from 'axios';
+// import { reset } from 'cli-color';
 
 function Register() {
+    const [name,setname]=useState("");
+    const [email,setemail]=useState("");
+    const [password,setpassword]=useState("");
+    const fetchnotes=async()=>{
+      const data=await axios.get("http://localhost:3000/api/rent")
+      .then((res)=>console.log(res.data))
+      // console.log("data"+data);
+    }
+    useEffect(()=>{
+fetchnotes();
+    },[])
     const handlesubmit=()=>{
-        
+
     }
   return (
     
     <form onSubmit={handlesubmit}>
         <p>name</p>
-        <input type="text" />
+        <input type="text"
+        value={name}
+        onChange={(e)=>{setname(e.target.value)}}
+        />
         <p>email</p>
-        <input type="email" />
+        <input type="email" 
+         value={email}
+         onChange={(e)=>{setemail(e.target.value)}} />
         <p>password</p>
-        <input type="password" />
+        <input type="password" 
+         value={password}
+         onChange={(e)=>{setpassword(e.target.value)}}/>
         <button type='submit'>register</button>
     </form>
   )
