a7ac390ba36cad84a9413519fcfc85b61da781e2
devyank <devyanknagpal2002@gmail.com>
2023-01-14 18:39:59 +0530

    filter styling

--- frontrent/src/components/Products.jsx
+++ frontrent/src/components/Products.jsx
@@ -1,15 +1,51 @@
 import React from 'react'
 import "../styles/product.css"
+import Form from 'react-bootstrap/Form';
 
 function Products() {
   return (
     <div className='outer'>
         <div className='leftbox'>
-            <div>
+            <div className="filter">
                 Product Type
+                <Form>
+      {['checkbox'].map((type) => (
+        <div key={`default-${type}`} className="mb-3">
+          <Form.Check 
+            type={type}
+            id={`default-${type}`}
+            label="bed"
+          />
+           <Form.Check 
+            type={type}
+            id={`default-${type}`}
+            label="sofa"
+          />
+        </div>
+      ))}
+    </Form>
             </div>
-            <div>
+            <div className="filter">
                 Range
+                {['checkbox'].map((type) => (
+        <div key={`default-${type}`} className="mb-3">
+          <Form.Check 
+            type={type}
+            id={`default-${type}`}
+            label="affordable"
+          />
+           <Form.Check 
+            type={type}
+            id={`default-${type}`}
+            label="medium"
+          />
+           <Form.Check 
+            type={type}
+            id={`default-${type}`}
+            label="luxury"
+          />
+        </div>
+      ))}
             </div>
         </div>
         <div className='rightbox'>
